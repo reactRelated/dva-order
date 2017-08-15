e4828ba68a70fad6f4e507e4aa0826306a1e9380
Mr Chen <236514977@qq.com>
2017-08-15 17:07:12 +0800

    init frame

--- .roadhogrc.mock.js
+++ .roadhogrc.mock.js
@@ -1,4 +1,5 @@
 const mock = {}
+//循环 mock 下的文件 导入请求拦截
 require('fs').readdirSync(require('path').join(__dirname + '/src/mock')).forEach(function(file) {
   Object.assign(mock, require('./src/mock/' + file))
 })
